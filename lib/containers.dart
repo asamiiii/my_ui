4abb447c86332c0a260b2569aa7430f96859a162
asamiiii <mahrmlolo@gmail.com>
2024-01-24 11:09:29 +0200

    second commit

---
+++ lib/containers.dart
@@ -0,0 +1,14 @@
+library my_ui;
+
+import 'package:flutter/widgets.dart';
+
+class ContainerWithRadius extends StatelessWidget {
+  const ContainerWithRadius({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
